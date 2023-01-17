ccd8edd42093875dd5733bd6c7e71ea2271a9315
Yaugen <yaugenkosach@gmail.com>
2023-01-18 01:12:40 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    const noMinus = Math.abs(n);
+    let nToStr = noMinus.toString();
+    let i = 0;
+    let result = '';
+    while (i < nToStr.length) {
+      result = `${nToStr[i]}${result}`;
+      i = i + 1;
+    }
   
+    return Number(result);
 }
